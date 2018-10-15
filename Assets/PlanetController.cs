fb83a37646ab6e2e7f4bbbbd224dfc2997006db5
Adam Borys <hipowolemia@gmail.com>
2018-10-16 00:10:45 +0200

    Multiple randomized orbit progresses.

--- Assets/PlanetController.cs
+++ Assets/PlanetController.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlanetController : MonoBehaviour {
 
-	[Range(0f,1f)]
-	public float OrbitProgress;
-
 	private GameObject system;
 	private SystemCreator systemCreator;
 	private List<Ellipse> orbitalPaths;
 	private List<Transform> planetTransforms;
+	private List<float> progresses;
+	private List<float> periods;
 	private GameObject prefab;
 
 	// Use this for initialization
@@ -20,6 +19,13 @@ public class PlanetController : MonoBehaviour {
 
 		orbitalPaths = new List<Ellipse>();
 		planetTransforms = new List<Transform>();
+		progresses = new List<float>();
+		periods = new List<float>();
+
+		System.Random rand = new System.Random();
+		for(int i = 0; i < systemCreator.PlanetQuantity; i++) {
+			progresses.Add((float)rand.NextDouble());
+		}
 		
 		prefab = Resources.Load("Prefabs/Planet") as GameObject;
 		foreach(GameObject orbit in systemCreator.Orbits){
@@ -33,18 +39,18 @@ public class PlanetController : MonoBehaviour {
 
 		for(int i = 0; i < orbitalPaths.Count; i++) {
 			planetTransforms[i].parent = systemCreator.Orbits[i].transform;
-			SetPosition(orbitalPaths[i], planetTransforms[i]);
+			SetPosition(orbitalPaths[i], planetTransforms[i], progresses[i]);
 		}
 	}
 	
 	void Update () {
 		for(int i = 0; i < orbitalPaths.Count; i++) {
-			SetPosition(orbitalPaths[i], planetTransforms[i]);
+			SetPosition(orbitalPaths[i], planetTransforms[i], progresses[i]);
 		}
 	}
 
-	public void SetPosition(Ellipse orbitalPath, Transform planetTransform) {
-		Vector2 position = orbitalPath.Evaluate(OrbitProgress);
+	public void SetPosition(Ellipse orbitalPath, Transform planetTransform, float progress) {
+		Vector2 position = orbitalPath.Evaluate(progress);
 		planetTransform.localPosition = new Vector3(position.x, position.y,0);
 	}
 }
