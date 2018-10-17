b439fefb6cf1c17cda4b28eafe13c53c93b6c9f4
Adam Borys <hipowolemia@gmail.com>
2018-10-17 10:29:22 +0200

    Linked orbital period with planet size and distance from star.

--- Assets/PlanetController.cs
+++ Assets/PlanetController.cs
@@ -8,8 +8,9 @@ public class PlanetController : MonoBehaviour {
 	private SystemCreator systemCreator;
 	private List<Ellipse> orbitalPaths;
 	private List<Transform> planetTransforms;
-	private List<float> progresses;
-	private List<float> periods;
+	private List<float> planetSizes;
+	private List<float> planetProgresses;
+	private List<float> orbitalPeriods;
 	private GameObject prefab;
 
 	// Use this for initialization
@@ -19,38 +20,52 @@ public class PlanetController : MonoBehaviour {
 
 		orbitalPaths = new List<Ellipse>();
 		planetTransforms = new List<Transform>();
-		progresses = new List<float>();
-		periods = new List<float>();
+		planetSizes = new List<float>();
+		planetProgresses = new List<float>();
+		orbitalPeriods = new List<float>();
+
+		prefab = Resources.Load("Prefabs/Planet") as GameObject;
 
 		System.Random rand = new System.Random();
 		for(int i = 0; i < systemCreator.PlanetQuantity; i++) {
-			progresses.Add((float)rand.NextDouble());
-		}
-		
-		prefab = Resources.Load("Prefabs/Planet") as GameObject;
-		foreach(GameObject orbit in systemCreator.Orbits){
-			OrbitProvider orbitProvider = orbit.GetComponent<OrbitProvider>();
+			planetSizes.Add((float)(0.6-(rand.NextDouble()/2)));
+			planetProgresses.Add((float)rand.NextDouble());
+			orbitalPeriods.Add((float)(planetSizes[i] * 100 * Mathf.Pow((i+1),2)));
+			
+			OrbitProvider orbitProvider = systemCreator.Orbits[i].GetComponent<OrbitProvider>();
 			Ellipse orbitPath = orbitProvider.OrbitShape;
 			orbitalPaths.Add(orbitPath);
 			GameObject planet = (Instantiate(prefab) as GameObject);
 			planet.name = "Planet";
+			planet.transform.localScale = new Vector3(planetSizes[i],planetSizes[i],planetSizes[i]);
 			planetTransforms.Add(planet.transform);
-		}
-
-		for(int i = 0; i < orbitalPaths.Count; i++) {
 			planetTransforms[i].parent = systemCreator.Orbits[i].transform;
-			SetPosition(orbitalPaths[i], planetTransforms[i], progresses[i]);
+			StartCoroutine("AnimateOrbit",i);
 		}
 	}
 	
-	void Update () {
+	void Update () {/*
 		for(int i = 0; i < orbitalPaths.Count; i++) {
-			SetPosition(orbitalPaths[i], planetTransforms[i], progresses[i]);
-		}
+			SetPosition(orbitalPaths[i], planetTransforms[i], planetProgresses[i]);
+		} */
 	}
 
 	public void SetPosition(Ellipse orbitalPath, Transform planetTransform, float progress) {
 		Vector2 position = orbitalPath.Evaluate(progress);
 		planetTransform.localPosition = new Vector3(position.x, position.y,0);
+	}
+
+	IEnumerator AnimateOrbit(int index) {
+		if(orbitalPeriods[index] < 0.1f) {
+			orbitalPeriods[index] = 0.1f;
+		}
+		while(true)
+		{
+			float orbitalSpeed = 1f / orbitalPeriods[index];
+			planetProgresses[index] += Time.deltaTime * orbitalSpeed;
+			planetProgresses[index] %= 1f;
+			SetPosition(orbitalPaths[index], planetTransforms[index], planetProgresses[index]);
+			yield return null;
+		}
 	}
 }
