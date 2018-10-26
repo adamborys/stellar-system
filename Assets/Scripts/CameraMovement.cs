75a588a92a196086a23a3befdea78e4b3df6b4a0
Adam Borys <hipowolemia@gmail.com>
2018-10-26 22:09:16 +0200

    Successfully restricted camera xAxis rotation without gimbal lock. Bad translation during selection. No follow.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,51 +4,52 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-  private Transform systemOriginTransform;
-  private Transform dummyTransform;
-  private float mouseSpeedX, mouseSpeedY, zoom;
-  private Vector3 previous;
-  // Start is called before the first frame update
+  public static bool IsCameraLocked = false;
+  public static Transform ObservedTransform;
+  public static Vector3 Offset = new Vector3();
+  private GameObject dummyCamera;
+  private float mouseRotationX, mouseRotationY, zoom;
+  
   void Start()
   {
-    systemOriginTransform = GameObject.Find("SystemOrigin").transform;
-    transform.LookAt(systemOriginTransform);
+    ObservedTransform = GameObject.Find("SystemOrigin").transform;
+    Offset = ObservedTransform.position;
+    transform.LookAt(ObservedTransform);
     if (StellarSystem.IsStarted)
     {
       transform.position = SystemCreator.EditorCameraPosition;
       transform.rotation = SystemCreator.EditorCameraRotation;
     }
   }
-
-  // Update is called once per frame
+  
   void Update()
   {
-    if (Input.GetMouseButton(2))
+    if (!IsCameraLocked)
     {
-      mouseSpeedX = Input.GetAxis("Mouse X");
-      transform.Translate(Vector3.right * -mouseSpeedX * 2);
-
-      Vector3 temporaryPosition = transform.position;
-      Quaternion temporaryRotation = transform.rotation;
-
-      mouseSpeedY = Input.GetAxis("Mouse Y");
-      transform.Translate(Vector3.up * -mouseSpeedY * 2);
-      transform.LookAt(systemOriginTransform);
-      if (transform.rotation.eulerAngles.x < 0 || 
-      (transform.rotation.eulerAngles.x > 80 && transform.rotation.eulerAngles.x < 290))
+      if (Input.GetMouseButton(2))
       {
-        transform.position = temporaryPosition;
-        transform.rotation = temporaryRotation;
+        dummyCamera = Instantiate(transform.gameObject);
+        Transform dummyTransform = dummyCamera.transform;
+        mouseRotationX = Input.GetAxis("Mouse X");
+        dummyTransform.Translate(Vector3.right * -mouseRotationX * 2);
+
+        mouseRotationY = Input.GetAxis("Mouse Y");
+        dummyTransform.Translate(Vector3.up * -mouseRotationY * 2);
+        dummyTransform.LookAt(ObservedTransform);
+        
+        float x = dummyTransform.rotation.eulerAngles.x;
+        if((0 <= x && x <= 70) || (290 <= x && x < 360))
+        {
+          transform.position = dummyTransform.position;
+          transform.rotation = dummyTransform.rotation;
+        }
+        Destroy(dummyCamera);
       }
-      transform.LookAt(systemOriginTransform);
-    }
-    if (Input.GetAxis("Mouse ScrollWheel") > 0 && Vector3.Distance(transform.position, new Vector3(0, 0, 0)) > 10f)
-    {
-      transform.position += transform.forward * Input.mouseScrollDelta.y * 2f;
-    }
-    if (Input.GetAxis("Mouse ScrollWheel") < 0 && Vector3.Distance(transform.position, new Vector3(0, 0, 0)) < 200f)
-    {
-      transform.position += transform.forward * Input.mouseScrollDelta.y * 2f;
+      if ((Input.GetAxis("Mouse ScrollWheel") > 0 && Vector3.Distance(transform.position, new Vector3(0, 0, 0)) > 10f) ||
+      (Input.GetAxis("Mouse ScrollWheel") < 0 && Vector3.Distance(transform.position, new Vector3(0, 0, 0)) < 200f))
+      {
+        transform.position += transform.forward * Input.mouseScrollDelta.y * 2f;
+      }
     }
   }
 }
--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SelectionController : MonoBehaviour
 {
+    private Vector3 translation;
+    private RaycastHit hit;
+    private Ray ray;
     void Update()
     {
-       
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        
-        if (Input.GetMouseButton(0) && Physics.Raycast(ray, out hit)) {
-            Debug.Log(hit.transform.gameObject.name);
-            Camera.main.transform.LookAt(hit.transform);
-        } 
+        if (Input.GetMouseButtonDown(0)) {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                translation = hit.transform.position - CameraMovement.ObservedTransform.position;
+                Camera.main.transform.Translate(translation);
+                CameraMovement.ObservedTransform = hit.transform;
+                Camera.main.transform.LookAt(CameraMovement.ObservedTransform); // do przeniesienia do CameraMovement 
+                CameraMovement.Offset = new Vector3();
+            }
+        }
     }
 }
